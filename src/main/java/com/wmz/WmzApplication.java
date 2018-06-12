4867b2cf3499fecb0096a0701e87f33e6ab52cbc
WMZ <wmz831@163.com>
2018-06-13 00:18:12 +0800

    Initial commit

---
+++ src/main/java/com/wmz/WmzApplication.java
@@ -0,0 +1,12 @@
+package com.wmz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WmzApplication.class, args);
+	}
+}
